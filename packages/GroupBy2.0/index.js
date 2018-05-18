bb545fa4273ea5e66a33f24b5caa77ac17254d0f
Redmond Tran <redmondtran@builddirect.com>
2018-05-18 12:57:48 -0700

    initial commit

---
+++ packages/GroupBy2.0/index.js
@@ -0,0 +1,9 @@
+import makeBetter from "myradaddon";
+
+export default callGroupBy = () => {
+    const init = {
+        "data": "Hello World",
+        "version": "1.0"
+    };
+    return makeBetter(init);
+};
---
+++ packages/MyRadAddon/index.js
@@ -0,0 +1,3 @@
+export default makeBetter = (input) => {
+    return input.addOn = "ADDON";
+};
